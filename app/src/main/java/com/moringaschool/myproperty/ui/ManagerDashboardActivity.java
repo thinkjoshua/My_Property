f28fd8cddce24c3e2089209fa7a6050fb1b7923d
MutheuLinet <muneenilinet@gmail.com>
2022-06-29 23:29:30 +0300

    Creating layouts for forms

---
+++ app/src/main/java/com/moringaschool/myproperty/ui/ManagerDashboardActivity.java
@@ -0,0 +1,14 @@
+package com.moringaschool.myproperty.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.moringaschool.myproperty.R;
+import android.os.Bundle;
+
+public class ManagerDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager_dashboard);
+    }
+}
