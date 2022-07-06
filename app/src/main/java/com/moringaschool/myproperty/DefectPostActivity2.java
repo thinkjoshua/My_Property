fa2b5cb16b538257624ce76266c4a936b94a70a0
jackie <bobbinnej@gmail.com>
2022-07-06 13:01:51 +0300

    Create add a tenant layout

---
+++ app/src/main/java/com/moringaschool/myproperty/DefectPostActivity2.java
@@ -0,0 +1,14 @@
+package com.moringaschool.myproperty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DefectPostActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_defect_post2);
+    }
+}
