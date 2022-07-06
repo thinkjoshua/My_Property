4d4e5512437d5c6a202a196b32a64b29b9274dd3
jackie <bobbinnej@gmail.com>
2022-07-06 10:05:11 +0300

    Create manager dashboard

---
+++ app/src/main/java/com/moringaschool/myproperty/ManagerDashboardActivity2.java
@@ -0,0 +1,14 @@
+package com.moringaschool.myproperty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManagerDashboardActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager_dashboard2);
+    }
+}
