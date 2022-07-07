736ae4154284e836f5597b282ba295bde6001c47
MutheuLinet <muneenilinet@gmail.com>
2022-07-07 09:24:52 +0300

    Add tenant login activity

---
+++ app/src/main/java/com/moringaschool/myproperty/ui/TenantLoginActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.myproperty.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.myproperty.R;
+
+public class TenantLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tenant_login);
+    }
+}
