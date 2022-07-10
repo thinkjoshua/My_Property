79295369764270d048577c8808591d9f0acdebc9
MutheuLinet <muneenilinet@gmail.com>
2022-07-10 11:30:34 +0300

    Add activity to display login options

--- app/src/main/java/com/moringaschool/myproperty/ui/SelectActivity.java
+++ app/src/main/java/com/moringaschool/myproperty/ui/SelectActivity.java
@@ -2,16 +2,34 @@ package com.moringaschool.myproperty.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.moringaschool.myproperty.R;
+import com.moringaschool.myproperty.databinding.ActivitySelectBinding;
 
 
 public class SelectActivity extends AppCompatActivity {
 
+    ActivitySelectBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
+        binding.managerRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(SelectActivity.this, LoginActivity.class);
+//                startActivity(intent);
+            }
+        });
+        binding.tenantLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SelectActivity.this, TenantLoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
