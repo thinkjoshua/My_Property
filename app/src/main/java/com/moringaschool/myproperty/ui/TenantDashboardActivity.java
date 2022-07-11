6e5673d146fa50a8fd49e0a160605f275d765e59
MutheuLinet <muneenilinet@gmail.com>
2022-07-11 08:31:41 +0300

    add tenant dashboard and click listeners

--- app/src/main/java/com/moringaschool/myproperty/ui/TenantDashboardActivity.java
+++ app/src/main/java/com/moringaschool/myproperty/ui/TenantDashboardActivity.java
@@ -2,15 +2,29 @@ package com.moringaschool.myproperty.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.moringaschool.myproperty.R;
+import com.moringaschool.myproperty.databinding.ActivityTenantDashboardBinding;
+import com.moringaschool.myproperty.databinding.ActivityTenantDefectBinding;
 
-public class ManagerDashboardActivity2 extends AppCompatActivity {
+public class TenantDashboardActivity extends AppCompatActivity {
 
+    ActivityTenantDashboardBinding mainBind;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_manager_dashboard2);
+        mainBind = ActivityTenantDashboardBinding.inflate(getLayoutInflater());
+        setContentView(mainBind.getRoot());
+
+        mainBind.maintenanceRequestCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TenantDashboardActivity.this, TenantDefectActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
