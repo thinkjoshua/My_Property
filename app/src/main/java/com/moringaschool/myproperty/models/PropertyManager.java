b13f547a0aa077a38f1796b7453c86861046a8f7
CharlesMuvaka <charles.uvaka@student.moringaschool@gmail.com>
2022-07-11 10:13:13 +0300

    remove unwanted member variables.

--- app/src/main/java/com/moringaschool/myproperty/models/PropertyManager.java
+++ app/src/main/java/com/moringaschool/myproperty/models/PropertyManager.java
@@ -4,29 +4,26 @@ import java.util.Objects;
 
 public class PropertyManager {
 
-    public int id;
-    public String manager_name ,phone_number,email,property_name,property_description;
+    private int id;
+    private String manager_name ,phone_number,email;
 
-
-    public PropertyManager(String manager_name, String phone_number, String email, String property_name, String property_description) {
+    public PropertyManager(String manager_name, String phone_number, String email) {
         this.manager_name = manager_name;
         this.phone_number = phone_number;
         this.email = email;
-        this.property_name = property_name;
-        this.property_description = property_description;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        PropertyManager that = (PropertyManager) o;
-        return Objects.equals(manager_name, that.manager_name) && Objects.equals(phone_number, that.phone_number) && Objects.equals(email, that.email) && Objects.equals(property_name, that.property_name) && Objects.equals(property_description, that.property_description);
+        PropertyManager manager = (PropertyManager) o;
+        return Objects.equals(manager_name, manager.manager_name) && Objects.equals(phone_number, manager.phone_number) && Objects.equals(email, manager.email);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(manager_name, phone_number, email, property_name, property_description);
+        return Objects.hash(manager_name, phone_number, email);
     }
 
     public int getId() {
@@ -59,21 +56,5 @@ public class PropertyManager {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getProperty_name() {
-        return property_name;
-    }
-
-    public void setProperty_name(String property_name) {
-        this.property_name = property_name;
-    }
-
-    public String getProperty_description() {
-        return property_description;
-    }
-
-    public void setProperty_description(String property_description) {
-        this.property_description = property_description;
     }
 }
